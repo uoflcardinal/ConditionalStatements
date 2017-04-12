f1f94ce5e61ddad7d19d649fdc7dcc72f3b88299
Brian Wood <uoflcardinalsalltheway@yahoo.com>
2017-04-12 13:21:21 -0400

    Update to Number Validation

--- Program.cs
+++ Program.cs
@@ -15,15 +15,17 @@ namespace ConditionalStatements
                 char yes;
 
                 string myName;
-                Console.WriteLine("Please type your name: ");
+                Console.Write("Please type your name: ");
                 myName = Console.ReadLine();
 
                 int input;
                 Console.Write("{0} please enter an integer between 1 and 100: ", myName);
-                input = int.Parse(Console.ReadLine());
 
-
-
+                while (!int.TryParse(Console.ReadLine(), out input))
+                {
+                    Console.WriteLine("{0} your entry is invalid as it is not a number", myName);
+                    continue;
+                }
                 if (input > 100)
                 {
                     Console.WriteLine("{0} your entry is invalid as the number {1} entered is greater than 100!", myName, input);
@@ -32,7 +34,7 @@ namespace ConditionalStatements
                 {
                     Console.WriteLine("{0} your entry is invalid as the number {1} entered is less than 1!", myName, input);
                 }
-                if (input % 2 != 0 && input >= 1 && input <= 100)
+                else if (input % 2 != 0 && input >= 1 && input <= 100)
                 {
                     Console.WriteLine("{0} your entry of {1} is an odd number.", myName, input);
                 }
@@ -44,7 +46,7 @@ namespace ConditionalStatements
                 {
                     Console.WriteLine("{0} your entry of {1} is an even number between 26 and 60.", myName, input);
                 }
-                else if (input % 2 == 0 && input < 100 && input> 60)
+                else if (input % 2 == 0 && input < 100 && input > 60)
                 {
                     Console.WriteLine("{0} your entry of {1} is an even number greater than 60.", myName, input);
                 }
@@ -52,6 +54,9 @@ namespace ConditionalStatements
                 {
                     Console.WriteLine("{0} your entry of {1} is an odd number greater than 60.", myName, input);
                 }
+
+
+
                 Console.WriteLine("Continue? (y/n)");
                 yes = char.Parse(Console.ReadLine());
 
@@ -64,8 +69,11 @@ namespace ConditionalStatements
                     return;
                 }
 
-            } while (true);
-        }
+               
 
+
+            } while (true);
+
+        }
     }
 }
